97e953c03d4601fe4c99c2ab3e89b0b9a7836540
dhanshrigaikwad140898@gmail.com <dhanshrigaikwad140898@gmail.com>
2022-07-28 10:53:47 +0530

    Added Controller

---
+++ repos/FirstWebProject/LMS.Web/Areas/Demos/Controllers/MyFirstDemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Web.Areas.Demos.Controllers
+{
+    [Area("Demos")]
+    public class MyFirstDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
--- repos/FirstWebProject/LMS.Web/Startup.cs
+++ repos/FirstWebProject/LMS.Web/Startup.cs
@@ -50,6 +50,14 @@ namespace LMS.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area}/{controller}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
