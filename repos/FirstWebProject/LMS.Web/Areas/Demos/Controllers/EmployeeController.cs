e966f331317d60322095daa384db434d11455c09
dhanshrigaikwad140898@gmail.com <dhanshrigaikwad140898@gmail.com>
2022-07-29 10:43:04 +0530

    added viewmodel

---
+++ repos/FirstWebProject/LMS.Web/Areas/Demos/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Web.Areas.Demos.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ repos/FirstWebProject/LMS.Web/Areas/Demos/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LMS.Web.Areas.Demos.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        public string EmployeeName {  get; set; }
+
+     
+
+
+    }
+}
